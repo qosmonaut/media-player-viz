3ea04acbe257a8bbba439527a83a90578c1918dc
Alen Roady <alenroady@gmail.com>
2025-11-21 13:54:30 +0100

    Adjust build step

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'source-map',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'index.html', to: 'index.html' },
+        { from: 'styles.css', to: 'styles.css' },
+      ],
+    }),
+  ],
 };
